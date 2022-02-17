07ba1a845d41aa8fe5aeec7f67bec47b25952886
Marcel5210 <99146665+Marcel5210@users.noreply.github.com>
2022-02-17 06:20:09 -0500

    Add files via upload
    P1HW2

---
+++ P1HW2_BrownMarcel.py
@@ -0,0 +1,30 @@
+
+#Figure out how many pizza's that need to be ordered
+#Feb 15,2021
+#CTI-110 P1HW2 - Pizza Order
+#Marcel Brown
+#
+
+Num_students = int(input('How many students do you want to order pizza for? '))
+
+Slices = Num_students * 3
+Pizzas = Num_students / 2
+
+print()
+
+print('----Pizza Order--------')
+
+print('Number of Students:', Num_students)
+
+print('Pizza Slices Needed:', Slices)
+
+print('Pizzas Needed:', Pizzas)
+print('-------------------------')
+
+
+
+
+
+
+
+
