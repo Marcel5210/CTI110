6f4d380b9dd808a038dca529f4d89c653c43311f
Marcel5210 <99146665+Marcel5210@users.noreply.github.com>
2022-03-31 22:22:17 -0400

    Add files via upload
    P4LAB2B

---
+++ P4LAB2B_Brown.py
@@ -0,0 +1,44 @@
+import turtle
+mb = turtle.Turtle()
+win = turtle.Screen()
+
+mb.pensize(15)
+mb.pencolor('blue')
+     
+
+mb.left(90)
+mb.forward(100)
+mb.right(135)
+mb.forward(50)
+mb.left(90)
+mb.forward(50)
+mb.right(135)
+mb.forward(100)
+
+mb.penup()
+mb.left(90)
+mb.forward(50)
+mb.pendown()
+
+
+mb.left(90)
+mb.forward(100)
+mb.right(90)
+mb.forward(50)
+mb.right(90)
+mb.forward(40)
+mb.right(45)
+mb.forward(15)
+mb.right(45)
+mb.forward(40)
+mb.backward(40)
+mb.right(225)
+mb.forward(15)
+mb.right(45)
+mb.forward(40)
+mb.right(90)
+mb.forward(50)
+
+win.mainloop()             
+
+
